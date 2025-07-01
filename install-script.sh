c00efbff972e5b8882004ffc78f29a388e8c8aa2
s <s@g.c>
2025-07-02 03:03:47 +1000

    autocommit

--- install-script.sh
+++ install-script.sh
@@ -1,16 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 set -e # instead of chaining &&s
 # cd / # this is on iso so doesn't matter where it is, will get deleted anyway
 # sudo git clone https://github.com/sourajit02/nixcfg
 # cd nixcfg
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount --yes-wipe-all-disks ./disks.nix
-sudo nixos-generate-config --no-filesystems --root /mnt
+sudo nixos-generate-config --no-filesystems --root /mnt --force
 sudo cp /mnt/etc/nixos/hardware-configuration.nix .
 sudo nixos-install --root /mnt -I nixos-config=./configuration.nix --no-root-passwd
 # interactive
 sudo passwd root
 sudo passwd s
-su - s -c "cd config && pwd"
+cd /users/s/config
+git clone https://github.com/sourajit02/nixcfg
+cd nixcfg
+sudo nixos-generate-config --show-hardware-config > hardware-configuration.nix
+# su - s -c "cd config && pwd"
 # su - s -c "git clone https://github.com/sourajit02/nixcfg"
 # su - s -c "cd nixcfg"
 # su - s -c "sudo nixos-generate-config --show-hardware-config > hardware-configuration.nix"
