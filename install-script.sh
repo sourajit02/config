9b24b137bee2791c4aa135b2d3ed095a2e14208d
s <s@g.c>
2025-07-02 01:52:54 +1000

    autocommit

--- install-script.sh
+++ install-script.sh
@@ -3,7 +3,7 @@ set -e # instead of chaining &&s
 # cd /
 # sudo git clone https://github.com/sourajit02/nixcfg
 # cd nixcfg
-sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount ./disks.nix
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount --yes-wipe-all-disks ./disks.nix
 sudo nixos-generate-config --no-filesystems --root /mnt
 sudo cp /mnt/etc/nixos/hardware-configuration.nix .
 sudo nixos-install --root /mnt -I nixos-config=./configuration.nix --no-root-passwd
