b4b69d1b0a58ce19d2266c223fe2f8e92ab297e9
s <s@g.c>
2025-07-01 23:30:33 +1000

    autocommit

--- configuration.nix
+++ configuration.nix
@@ -4,9 +4,11 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    "${builtins.fetchTarball "https://github.com/nix-community/disko/archive/master.tar.gz"}/module.nix"
     # ./disks.nix
   ];
 
+  users.users.root.initialHashedPassword = "";
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   networking.hostName = "hbox";
