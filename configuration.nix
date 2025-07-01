595236091affec4001b793d6f943e04b90ab8282
s <s@g.c>
2025-07-01 21:25:48 +1000

    autocommit

---
+++ configuration.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+  ];
+
+  # ......
+
+  # Enable the Flakes feature and the accompanying new nix command-line tool
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+  environment.systemPackages = with pkgs; [
+    # Flakes clones its dependencies through the git command,
+    # so git must be installed first
+    git
+    nvim
+    helix
+    wget
+  ];
+  # Set the default editor to vim
+  environment.variables.EDITOR = "helix";
+
+  # ......
+}
---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "test config";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+  outputs =
+    { self, nixpkgs }:
+    {
+
+      # Please replace my-nixos with your hostname
+      nixosConfigurations.hbox = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+      };
+    };
+}
