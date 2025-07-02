2902b202f95fb89d9b139f76614e0731fe118dce
s <s@g.c>
2025-07-02 20:29:39 +1000

    autocommit

--- flake.nix
+++ flake.nix
@@ -14,13 +14,13 @@
     {
 
       # run this
-      # nix run github:nix-community/nixos-anywhere -- --flake .#vbox --generate-hardware-config nixos-generate-config ./hosts/vbox/hardware-configuration.nix hbox
+      # nix run github:nix-community/nixos-anywhere -- --flake .#hbox --generate-hardware-config nixos-generate-config ./hosts/hbox/hardware-configuration.nix hbox
       nixosConfigurations.hbox = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           disko.nixosModules.disko
-          ./hosts/vbox/configuration.nix
-          ./hosts/vbox/hardware-configuration.nix
+          ./hosts/hbox/configuration.nix
+          ./hosts/hbox/hardware-configuration.nix
         ];
       };
     };
