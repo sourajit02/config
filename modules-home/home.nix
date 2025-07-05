5895e34a5582e79bd54e06bb2169fc7432642bcd
s <s@g.c>
2025-07-05 23:08:18 +1000

    autocommit

--- modules-home/home.nix
+++ modules-home/home.nix
@@ -21,6 +21,32 @@
     dataHome = "/users/s/state/local/share";
     stateHome = "/users/s/state/local/state";
   };
+
+  programs.helix = {
+    enable = true;
+    settings = {
+      theme = "autumn_night_transparent";
+      editor.cursor-shape = {
+        normal = "block";
+        insert = "bar";
+        select = "underline";
+      };
+    };
+    languages.language = [
+      {
+        name = "nix";
+        auto-format = true;
+        formatter.command = lib.getExe pkgs.nixfmt-rfc-style;
+      }
+    ];
+    themes = {
+      autumn_night_transparent = {
+        "inherits" = "autumn_night";
+        "ui.background" = { };
+      };
+    };
+  };
+
   # programs.nushell = {
   #   enable = true;
   #   configFile.text = ''
