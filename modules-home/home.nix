18c6a54e071228d7f2c51e45cc017bfa9da428d3
s <s@g.c>
2025-07-05 19:40:58 +1000

    autocommit

--- modules-home/home.nix
+++ modules-home/home.nix
@@ -9,7 +9,7 @@
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
   home.username = "s";
-  home.homeDirectory = "/users/s/home";
+  # home.homeDirectory = "/users/s/home";
   # home.profileDirectory = "/users/s/config";
   # home.sessionVariables = {
   #   HOME_MANAGER_CONFIG = "/users/s/config";
