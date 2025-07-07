41527939fc43c422c72503373d67b4ceb63bb734
s <s@g.c>
2025-07-08 03:16:21 +1000

    autocommit

--- hosts/hbox/configuration.nix
+++ hosts/hbox/configuration.nix
@@ -91,32 +91,33 @@
   };
 
   boot.initrd.postResumeCommands = lib.mkAfter ''
-    mkdir /btrfs_tmp
-    mount /dev/sda /btrfs_tmp
-    if [[ -e /btrfs_tmp/root ]]; then
-        mkdir -p /btrfs_tmp/old_roots
-        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-        mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-    fi
+    echo "DEBUG: postResumeCommands starting" >> /dev/kmsg
+      mkdir /btrfs_tmp
+      mount /dev/sda /btrfs_tmp
 
-    if [[ -d /btrfs_tmp/root/TEST ]]; then
+      echo "DEBUG: Contents of /btrfs_tmp:" >> /dev/kmsg
+      ls -la /btrfs_tmp >> /dev/kmsg
+
+      if [[ -d /btrfs_tmp/root/TEST ]]; then
+        echo "DEBUG: Found /TEST, attempting to remove" >> /dev/kmsg
         rm -rf /btrfs_tmp/root/TEST
-    fi
+        echo "DEBUG: /TEST removal attempted" >> /dev/kmsg
+      fi
 
-    delete_subvolume_recursively() {
-        IFS=$'\n'
-        for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-            delete_subvolume_recursively "/btrfs_tmp/$i"
-        done
-        btrfs subvolume delete "$1"
-    }
+      delete_subvolume_recursively() {
+          IFS=$'\n'
+          for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+              delete_subvolume_recursively "/btrfs_tmp/$i"
+          done
+          btrfs subvolume delete "$1"
+      }
 
-    for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
-        delete_subvolume_recursively "$i"
-    done
+      for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+          delete_subvolume_recursively "$i"
+      done
 
-    btrfs subvolume create /btrfs_tmp/root
-    umount /btrfs_tmp
+      btrfs subvolume create /btrfs_tmp/root
+      umount /btrfs_tmp
   '';
 
   environment.persistence."/persist" = {
