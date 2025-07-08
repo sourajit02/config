e1d8dde9069ecacafb78e990bc0452e0881bb0bf
s <s@g.c>
2025-07-08 18:57:42 +1000

    autocommit

--- hosts/hbox/configuration.nix
+++ hosts/hbox/configuration.nix
@@ -91,45 +91,45 @@
   };
 
   boot.initrd.postResumeCommands = lib.mkAfter ''
-    echo "DEBUG: postResumeCommands starting" >> /dev/kmsg
-      mkdir /btrfs_tmp
-      mount /dev/disk/by-partlabel/disk-primary-internal /btrfs_tmp
-      
-      if [[ -e /btrfs_tmp/root ]]; then
-        mkdir -p /btrfs_tmp/old_roots
-        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-        mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-      fi
+    mkdir /btrfs_tmp
+    mount /dev/disk/by-partlabel/disk-primary-internal /btrfs_tmp
+
+    if [[ -e /btrfs_tmp/root ]]; then
+      mkdir -p /btrfs_tmp/old_roots
+      timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+      mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+    fi
 
 
-      if [[ -e /btrfs_tmp/home ]]; then
-        mkdir -p /btrfs_tmp/old_homes
-        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/home)" "+%Y-%m-%-d_%H:%M:%S")
-        mv /btrfs_tmp/home "/btrfs_tmp/old_homes/$timestamp"
-      fi      
+    if [[ -e /btrfs_tmp/home ]]; then
+      mkdir -p /btrfs_tmp/old_homes
+      timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/home)" "+%Y-%m-%-d_%H:%M:%S")
+      mv /btrfs_tmp/home "/btrfs_tmp/old_homes/$timestamp"
+    fi      
 
-      delete_subvolume_recursively() {
-          IFS=$'\n'
-          for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-              delete_subvolume_recursively "/btrfs_tmp/$i"
-          done
-          btrfs subvolume delete "$1"
-      }
+    delete_subvolume_recursively() {
+        IFS=$'\n'
+        for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+            delete_subvolume_recursively "/btrfs_tmp/$i"
+        done
+        btrfs subvolume delete "$1"
+    }
 
-      for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
-          delete_subvolume_recursively "$i"
-      done
+    for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+        delete_subvolume_recursively "$i"
+    done
 
-      for i in $(find /btrfs_tmp/old_homes/ -maxdepth 1 -mtime +30); do
-          delete_subvolume_recursively "$i"
-      done      
+    for i in $(find /btrfs_tmp/old_homes/ -maxdepth 1 -mtime +30); do
+        delete_subvolume_recursively "$i"
+    done      
 
-      btrfs subvolume create /btrfs_tmp/root
-      
-      btrfs subvolume create /btrfs_tmp/home
-      chown s:users /btrfs_tmp/home/s
-      
-      umount /btrfs_tmp
+    btrfs subvolume create /btrfs_tmp/root
+
+    btrfs subvolume create /btrfs_tmp/home
+    btrfs subvolume create /btrfs_tmp/home/s
+    sudo chown s:users /btrfs_tmp/home/s
+
+    umount /btrfs_tmp
   '';
 
   environment.persistence."/persist" = {
