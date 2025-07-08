1387a013df7ba8af99e318fb68381ff8c308bb6d
s <s@g.c>
2025-07-08 21:06:12 +1000

    autocommit

--- modules-home/home.nix
+++ modules-home/home.nix
@@ -7,24 +7,7 @@
 }:
 
 {
-
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  # home.username = "s";
-  # home.homeDirectory = /users/s/state;
   home.preferXdgDirectories = true;
-  # home.profileDirectory = "/users/s/state";
-  # home.sessionVariables = {
-  #   HOME_MANAGER_CONFIG = "/users/s/config";
-  # };
-  # nix.settings.use-xdg-base-directories = true; # This will use XDG directories
-  # xdg = {
-  #   enable = true;
-  #   configHome = /users/s/state/.config;
-  #   cacheHome = /users/s/state/cache;
-  #   dataHome = /users/s/home/.local/share;
-  #   stateHome = /users/s/home/.local/state;
-  # };
   programs.nushell = {
     enable = true;
     extraConfig = ''
@@ -34,7 +17,6 @@
             sudo nixos-rebuild switch --flake
           }
     '';
-
     settings = {
       show_banner = false;
       completions.external = {
@@ -47,6 +29,7 @@
       history.isolation = false;
     };
   };
+
   programs.yazi = {
     enable = true;
   };
@@ -79,39 +62,6 @@
     };
   };
 
-  # programs.nushell = {
-  #   enable = true;
-  #   configFile.text = ''
-  #     # Set HOME to the clean home directory after nushell loads
-  #     $env.HOME = "/users/s/home"
-
-  #     # Launch Niri if no display is set (console login)
-  #     if ($env.DISPLAY? | is-empty) {
-  #       cd $env.HOME
-  #       niri
-  #     }
-  #   '';
-
-  #   envFile.text = ''
-  #     # Initial HOME for nushell to load its config
-  #     $env.HOME = "/users/s/home"
-  #     # Set other XDG directories
-  #     $env.XDG_CONFIG_HOME = "/users/s/config"
-  #     $env.XDG_CACHE_HOME = "/users/s/config/cache"
-  #     $env.XDG_DATA_HOME = "/users/s/config/local/share"
-  #     $env.XDG_STATE_HOME = "/users/s/config/local/state"
-  #   '';
-  # };
-
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
   home.stateVersion = "25.11";
-  # Let Home Manager install and manage itself.
-  # programs.home-manager.enable = true;
+  programs.home-manager.enable = true;
 }
