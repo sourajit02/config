f1ad1e4bba6261ee97e857a2f8baf1101bcdecc3
s <s@g.c>
2025-07-12 14:24:52 +1000

    autocommit

--- hosts/hbox/configuration.nix
+++ hosts/hbox/configuration.nix
@@ -89,72 +89,6 @@
     createHome = true;
   };
 
-  # subvolumes other than root will still be persisted
-  # check disko for what they are
-
-  # boot.initrd.systemd.services.btrfs-root-cleanup = {
-  #   description = "Clean up old btrfs root snapshots";
-  #   wantedBy = [ "initrd.target" ];
-  #   after = [ "systemd-udev-settle.service" ];
-  #   before = [ "sysroot.mount" ];
-  #   unitConfig.DefaultDependencies = false;
-  #   serviceConfig = {
-  #     Type = "oneshot";
-  #     RemainAfterExit = true;
-  #   };
-  #   script = ''
-  #     mkdir -p /btrfs_tmp
-  #     mount /dev/disk/by-partlabel/disk-primary-internal /btrfs_tmp
-
-  #     if [[ -e /btrfs_tmp/root ]]; then
-  #       mkdir -p /btrfs_tmp/old_roots
-  #       timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-  #       mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-  #     fi
-
-  #     delete_subvolume_recursively() {
-  #         IFS=$'\n'
-  #         for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-  #             delete_subvolume_recursively "/btrfs_tmp/$i"
-  #         done
-  #         btrfs subvolume delete "$1"
-  #     }
-
-  #     for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +7); do
-  #         delete_subvolume_recursively "$i"
-  #     done
-
-  #     btrfs subvolume create /btrfs_tmp/root
-  #     umount /btrfs_tmp
-  #   '';
-  # };
-
-  # boot.initrd.postResumeCommands = lib.mkAfter ''
-  #   mkdir /btrfs_tmp
-  #   mount /dev/disk/by-partlabel/disk-primary-internal /btrfs_tmp
-
-  #   if [[ -e /btrfs_tmp/root ]]; then
-  #     mkdir -p /btrfs_tmp/old_roots
-  #     timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
-  #     mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
-  #   fi
-
-  #   delete_subvolume_recursively() {
-  #       IFS=$'\n'
-  #       for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
-  #           delete_subvolume_recursively "/btrfs_tmp/$i"
-  #       done
-  #       btrfs subvolume delete "$1"
-  #   }
-
-  #   for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +7); do
-  #       delete_subvolume_recursively "$i"
-  #   done
-
-  #   btrfs subvolume create /btrfs_tmp/root
-  #   umount /btrfs_tmp
-  # '';
-
   boot.initrd.systemd.enable = true;
   preservation = {
     enable = true;
@@ -211,7 +145,7 @@
             directory = ".local/share/Trash";
             # how = "bindmount";
             # mountOptions = [ "x-gvfs-trash" ];
-            # how = "symlink";
+            how = "symlink";
             mode = "0777";
             # configureParent = true;
             # parent.user = "s";
@@ -242,33 +176,4 @@
       "systemd-machine-id-setup --commit --root /persistent"
     ];
   };
-
-  # environment.persistence."/persist" = {
-  #   hideMounts = true;
-  #   directories = [
-  #     "/var/lib/nixos"
-  #     "/var/lib/bluetooth"
-  #     "/var/lib/systemd/coredump"
-  #     "/var/lib/systemd/timers"
-  #     "/etc/NetworkManager/system-connections"
-  #     # {
-  #     #   directory = "/var/lib/colord";
-  #     #   user = "colord";
-  #     #   group = "colord";
-  #     #   mode = "u=rwx,g=rx,o=";
-  #     # }
-  #   ];
-  #   files = [
-
-  #     # "/etc/machine-id"
-  #     # {
-  #     #   file = "/etc/nix/id_rsa";
-  #     #   parentDirectory = {
-  #     #     mode = "u=rwx,g=,o=";
-  #     #   };
-  #     # }
-
-  #   ];
-  #   # do not use home-manager's impermanence module as it comes with fuse performance penalty
-  # };
 }
