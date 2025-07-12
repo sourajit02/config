4b76f68dff07bc503a388de18e05e1768983a560
s <s@g.c>
2025-07-13 02:34:16 +1000

    autocommit

--- hosts/hbox/configuration.nix
+++ hosts/hbox/configuration.nix
@@ -82,6 +82,7 @@
     isNormalUser = true;
     # initialPassword = "password";
     hashedPassword = "$y$j9T$PS95D2rMVAwSx4Ib50mTl/$MwcFGKV68bdc7F.U1K4anWN3hbUaTVzXofp0viSFRn.";
+    initialHashedPassword = "$y$j9T$PS95D2rMVAwSx4Ib50mTl/$MwcFGKV68bdc7F.U1K4anWN3hbUaTVzXofp0viSFRn.";
     shell = pkgs.nushell;
     extraGroups = [
       "wheel"
