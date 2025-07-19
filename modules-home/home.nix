c5e7e94478866fd92978b03e119894ee04fedcf9
sourajit <56763768+sourajit02@users.noreply.github.com>
2025-07-19 19:56:57 +1000

    autocommit

--- modules-home/home.nix
+++ modules-home/home.nix
@@ -12,8 +12,8 @@
   ];
 
   # nixpkgs.overlays = [ inputs.niri.overlays.niri ];
-  programs.niri.package = pkgs.niri-unstable;
-  # environment.variables.NIXOS_OZONE_WL = "1";
+  # programs.niri.package = pkgs.niri-unstable;
+  environment.variables.NIXOS_OZONE_WL = "1";
 
   home.packages = with pkgs; [
     waybar
