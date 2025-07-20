cff2bbff6290e081c2c65f1a53c84a12dc486c90
sourajit <56763768+sourajit02@users.noreply.github.com>
2025-07-20 13:47:18 +1000

    autocommit

--- hosts/hbox/configuration.nix
+++ hosts/hbox/configuration.nix
@@ -198,8 +198,8 @@
   stylix.enable = true;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
   stylix.image = pkgs.fetchurl {
-    url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
-    hash = "sha256-enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
+    url = "https://4kwallpapers.com/images/wallpapers/genshin-impact-5120x2880-22945.jpg";
+    # hash = "sha256-enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
   };
 
 }
