8303b3bbe6de898d2157682514b83db7dc509063
sourajit <56763768+sourajit02@users.noreply.github.com>
2025-07-24 21:25:45 +1000

    autocommit

--- hosts/hbox/configuration.nix
+++ hosts/hbox/configuration.nix
@@ -367,10 +367,21 @@
     };
   };
 
+  ## accessed at localhost:28981
   # environment.etc."paperless-admin-pass".text = "admin";
   # services.paperless = {
   #   enable = true;
   #   passwordFile = "/etc/paperless-admin-pass";
+
+  #   PAPERLESS_CONSUMER_IGNORE_PATTERN = [
+  #     ".DS_STORE/*"
+  #     "desktop.ini"
+  #   ];
+  #   PAPERLESS_OCR_LANGUAGE = "deu+eng";
+  #   PAPERLESS_OCR_USER_ARGS = {
+  #     optimize = 1;
+  #     pdfa_image_compression = "lossless";
+  #   };
   # };
 
 }
