4dbd95d5bd102b701fa3623e1b19b9e37cdb37f8
sourajit <56763768+sourajit02@users.noreply.github.com>
2025-07-24 23:27:37 +1000

    autocommit

--- hosts/hbox/configuration.nix
+++ hosts/hbox/configuration.nix
@@ -380,11 +380,11 @@
     #   ".DS_STORE/*"
     #   "desktop.ini"
     # ];
-    PAPERLESS_OCR_LANGUAGE = "deu+eng";
-    PAPERLESS_OCR_USER_ARGS = {
-      optimize = 1;
-      pdfa_image_compression = "lossless";
-    };
+    # PAPERLESS_OCR_LANGUAGE = "deu+eng";
+    # PAPERLESS_OCR_USER_ARGS = {
+    #   optimize = 1;
+    #   pdfa_image_compression = "lossless";
+    # };
   };
 
 }
