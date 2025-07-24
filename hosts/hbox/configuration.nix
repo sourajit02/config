2991fa69b334e65e69d31779fd61a488402ff97b
sourajit <56763768+sourajit02@users.noreply.github.com>
2025-07-24 21:24:17 +1000

    autocommit

--- hosts/hbox/configuration.nix
+++ hosts/hbox/configuration.nix
@@ -366,10 +366,11 @@
       };
     };
   };
-  # stylix.targets.firefox.profileNames = [ ];
 
-  services.paperless = {
-    enable = true;
-  };
+  # environment.etc."paperless-admin-pass".text = "admin";
+  # services.paperless = {
+  #   enable = true;
+  #   passwordFile = "/etc/paperless-admin-pass";
+  # };
 
 }
